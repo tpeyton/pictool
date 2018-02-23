bf3e51a6bb1ba7335657a135c9aa593d47e7e5f3
tpeyton <tpeyton25@gmail.com>
2018-02-23 14:24:50 -0500

    updated xml parsing

--- getAuthInfo.py
+++ getAuthInfo.py
@@ -5,17 +5,28 @@
 import requests
 import xml.etree.ElementTree as ET
 
-server = 192.168.1.100
+server = "192.168.1.100"
 
 # get xml file from server (note for actual use, port is 49152)
-r = request.get('http://' + server + '/CameraDevDesc.xml')
+r = requests.get('http://' + server + '/CameraDevDesc.xml')
 
 if r.status_code != requests.codes.ok:
     exit("Unable to obtain device information, ensure camera is in pairing mode.")
+else:
+    print("Successfully retrieved info from " + server + "\n")
 
 # Parse returned xml data
 root = ET.fromstring(r.text)
 
-guid = root.find('UDN')
+#for child in root:
+#    print(child.tag, child.attrib)
 
-print guid.attrib
+#print(root[2][7].text)
+
+for elem in root:
+    for subelem in elem:
+        print(subelem.tag, subelem.text)
+
+
+
+#print(guid)
