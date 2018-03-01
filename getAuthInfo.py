647aa4bdfe0f8329637fd569f82ae5bd81f2d595
tpeyton <tpeyton25@gmail.com>
2018-03-01 13:26:58 -0500

    updated to support getting device nickname

--- getAuthInfo.py
+++ getAuthInfo.py
@@ -19,13 +19,15 @@ else:
 root = ET.fromstring(r.text)
 
 # reference for XML parsing: https://eli.thegreenplace.net/2012/03/15/processing-xml-in-python-with-elementtree
+# another one: https://docs.python.org/3/library/xml.etree.elementtree.html
 #tree = ET.parse('CameraDevDesc.xml')
 #root = tree.getroot()
 
-
+# Retrieve relevant fields
 uuid = root.find('{urn:schemas-upnp-org:device-1-0}device/{urn:schemas-upnp-org:device-1-0}UDN').text
-
+nickName = root.find('{urn:schemas-upnp-org:device-1-0}device/{urn:schemas-upnp-org:device-1-0}serviceList/{urn:schemas-upnp-org:device-1-0}service/{urn:schemas-canon-com:schema-upnp}X_deviceNickname').text
 friendlyName = root.find('{urn:schemas-upnp-org:device-1-0}device/{urn:schemas-upnp-org:device-1-0}friendlyName').text
 
-print(uuid)
-print(friendlyName)
+print("uuid: " + uuid)
+print("Friendly Name" + friendlyName)
+print("Device Nickname" + nickName)
