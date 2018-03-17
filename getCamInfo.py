ec3487ccaaedd5f0f4ec77b9519db4ee09315bd4
tpeyton <tpeyton25@gmail.com>
2018-03-17 23:25:00 +0000

    refined to work with real camera

---
+++ getCamInfo.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+# This script retrieves UPnP configuration info and will parse the file for the PTP authentication data
+
+# Import Dependencies
+import requests
+import xml.etree.ElementTree as ET
+
+# initialize variables
+server = "10.0.0.2"
+
+# get xml file from server (note for actual use, port is 49152)
+r = requests.get('http://' + server + ':49152/upnp/CameraDevDesc.xml')
+
+# attempt to get file
+if r.status_code != requests.codes.ok:
+    print("http code: {}".format(r.status_code))
+    exit("Unable to obtain device information, ensure camera is in pairing mode.")
+else:
+    print("Successfully retrieved info from " + server + "\n")
+
+# Parse returned xml data
+root = ET.fromstring(r.text)
+
+# Retrieve relevant fields
+uuid = root.find('{urn:schemas-upnp-org:device-1-0}device/{urn:schemas-upnp-org:device-1-0}UDN').text
+nickName = root.find('{urn:schemas-upnp-org:device-1-0}device/{urn:schemas-upnp-org:device-1-0}serviceList/{urn:schemas-upnp-org:device-1-0}service/{urn:schemas-canon-com:schema-upnp}X_deviceNickname').text
+friendlyName = root.find('{urn:schemas-upnp-org:device-1-0}device/{urn:schemas-upnp-org:device-1-0}friendlyName').text
+
+# note [5:] strips the first 5 chars from uuid which are 'uuid:'
+print("uuid: " + uuid[5:])
+print("Friendly Name: " + friendlyName)
+print("Device Nickname: " + nickName)
