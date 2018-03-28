fc7526fbaccfbc568ffbbc1736e1127a0013b2f9
tpeyton <tpeyton25@gmail.com>
2018-03-28 15:19:44 -0400

    initial commit for ap connect script

---
+++ connectToAP.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+run airodump-ng on correct channel and mac n shit
+if station is blank
+    print client is not connected, waiting for client to connected
+else
+    print client is already connected, attempting deauth
+    aireplay-ng client
+while(aircrack exits with "no valid handshakes found, or no file or directory")
+
+
+# take captured cap and format for hashcat (old)
+aircrack-ng psk-01.cap -J output
+
+# take captured cap and format for hashcat 4
+cap2hccapx.bin /path/to/psk-01.cap /path/to/output.hccapx
+
+# call hashcat remotely
+    # output the found key
+
+# authenticate to the AP
